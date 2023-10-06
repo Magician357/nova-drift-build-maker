6a1d06ba14a61a30c7d7cc044292bb2a5034412e
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-10-06 13:48:36 -0700

    you know, cause you did the changes

---
+++ mods.py
@@ -0,0 +1,127 @@
+# https://nova-drift.fandom.com/wiki/Mods_Overview
+
+# Gear
+bodies={
+    "Architect":"https://static.wikia.nocookie.net/nova-drift/images/f/fb/Architect.png/revision/latest/scale-to-width-down/100?cb=20191011010925",
+    "Assault":"https://static.wikia.nocookie.net/nova-drift/images/e/e0/Assault.png/revision/latest/scale-to-width-down/100?cb=20191011010930",
+    "Battery":"https://static.wikia.nocookie.net/nova-drift/images/6/6d/Battery.png/revision/latest/scale-to-width-down/100?cb=20191011010935",
+    "Carrier":"https://static.wikia.nocookie.net/nova-drift/images/b/b7/Carrier.png/revision/latest/scale-to-width-down/100?cb=20191226091451",
+    "Courser":"https://static.wikia.nocookie.net/nova-drift/images/9/9b/Courser.png/revision/latest/scale-to-width-down/100?cb=20191011010946",
+    "Engineer":"https://static.wikia.nocookie.net/nova-drift/images/d/d8/Engineer.png/revision/latest/scale-to-width-down/100?cb=20191011010952",
+    "Firefly":"https://static.wikia.nocookie.net/nova-drift/images/d/d7/Firefly.png/revision/latest/scale-to-width-down/100?cb=20191115103311",
+    "Hullbreaker":"https://static.wikia.nocookie.net/nova-drift/images/1/17/Hullbreaker.png/revision/latest/scale-to-width-down/100?cb=20191011010957",
+    "Research":"https://static.wikia.nocookie.net/nova-drift/images/2/20/Research.png/revision/latest/scale-to-width-down/100?cb=20191011011002",
+    "Sentinel":"https://static.wikia.nocookie.net/nova-drift/images/9/91/Sentinel.png/revision/latest/scale-to-width-down/100?cb=20191011011007",
+    "Leviathan":"https://static.wikia.nocookie.net/nova-drift/images/0/0a/Leviathan.png/revision/latest/scale-to-width-down/100?cb=20200921205007",
+    "Spectre":"https://static.wikia.nocookie.net/nova-drift/images/9/90/Spectre.png/revision/latest/scale-to-width-down/100?cb=20191115103317",
+    "Standard Body":"https://static.wikia.nocookie.net/nova-drift/images/4/45/Standard_Body.png/revision/latest/scale-to-width-down/100?cb=20191011011011",
+    "Viper":"https://static.wikia.nocookie.net/nova-drift/images/9/99/Viper.png/revision/latest/scale-to-width-down/100?cb=20191011011020",
+}
+
+weapons={
+    "Blade":"https://static.wikia.nocookie.net/nova-drift/images/a/ad/Blade.png/revision/latest/scale-to-width-down/100?cb=20191011013410",
+    "Blaster":"https://static.wikia.nocookie.net/nova-drift/images/3/3d/Blaster.png/revision/latest/scale-to-width-down/100?cb=20191011013416",
+    "Dart":"https://static.wikia.nocookie.net/nova-drift/images/2/29/Dart.png/revision/latest/scale-to-width-down/100?cb=20191011013419",
+    "Flak":"https://static.wikia.nocookie.net/nova-drift/images/3/3c/Flak.png/revision/latest/scale-to-width-down/100?cb=20191011013425",
+    "Grenade":"https://static.wikia.nocookie.net/nova-drift/images/f/fb/Grenade.png/revision/latest/scale-to-width-down/100?cb=20191011013431",
+    "Pulse":"https://static.wikia.nocookie.net/nova-drift/images/a/a9/Pulse.png/revision/latest/scale-to-width-down/100?cb=20191011013435",
+    "Railgun":"https://static.wikia.nocookie.net/nova-drift/images/a/ab/Railgun.png/revision/latest/scale-to-width-down/100?cb=20191011013439",
+    "Salvo":"https://static.wikia.nocookie.net/nova-drift/images/d/df/Salvo.png/revision/latest/scale-to-width-down/100?cb=20191011013444",
+    "Split Shot":"https://static.wikia.nocookie.net/nova-drift/images/0/04/Split_Shot.png/revision/latest/scale-to-width-down/100?cb=20191011013449",
+    "Swords":"https://static.wikia.nocookie.net/nova-drift/images/9/96/Swords.png/revision/latest/scale-to-width-down/100?cb=20230925180516",
+    "Thermal Lance":"https://static.wikia.nocookie.net/nova-drift/images/a/a4/Thermal_Lance.png/revision/latest/scale-to-width-down/100?cb=20200214110120",
+    "Torrent":"https://static.wikia.nocookie.net/nova-drift/images/7/7a/Torrent.png/revision/latest/scale-to-width-down/100?cb=20191011013454",
+    "Vortex":"https://static.wikia.nocookie.net/nova-drift/images/5/52/Vortex.png/revision/latest/scale-to-width-down/100?cb=20191011013458"
+}
+
+shields={
+    "Amp":"https://static.wikia.nocookie.net/nova-drift/images/f/f8/Amp.png/revision/latest/scale-to-width-down/100?cb=20191011010450",
+    "Bastion":"https://static.wikia.nocookie.net/nova-drift/images/b/bf/Bastion.png/revision/latest/scale-to-width-down/100?cb=20191011010456",
+    "Halo":"https://static.wikia.nocookie.net/nova-drift/images/f/f3/Halo.png/revision/latest/scale-to-width-down/100?cb=20191011010501",
+    "Orbital":"https://static.wikia.nocookie.net/nova-drift/images/9/98/Orbital.png/revision/latest/scale-to-width-down/100?cb=20200921205040",
+    "Reflect":"https://static.wikia.nocookie.net/nova-drift/images/d/d0/Reflect.png/revision/latest/scale-to-width-down/100?cb=20191011010511",
+    "Shockwave":"https://static.wikia.nocookie.net/nova-drift/images/6/65/Shockwave.png/revision/latest/scale-to-width-down/100?cb=20200225113717",
+    "Siphon":"https://static.wikia.nocookie.net/nova-drift/images/3/3a/Siphon.png/revision/latest/scale-to-width-down/100?cb=20191011010515",
+    "Standard Shield":"https://static.wikia.nocookie.net/nova-drift/images/6/6b/Standard_Shield.png/revision/latest/scale-to-width-down/100?cb=20191011010521",
+    "Temporal":"https://static.wikia.nocookie.net/nova-drift/images/a/a4/Temporal.png/revision/latest/scale-to-width-down/100?cb=20191011010525",
+    "Warp":"https://static.wikia.nocookie.net/nova-drift/images/a/a6/Warp.png/revision/latest/scale-to-width-down/100?cb=20191011010529"
+}
+
+# Mods
+mods={
+    "velocity":{
+        "Velocity":"https://static.wikia.nocookie.net/nova-drift/images/0/0f/Velocity.png/revision/latest/scale-to-width-down/100?cb=20191011023943",
+        "Snipe":"https://static.wikia.nocookie.net/nova-drift/images/4/40/Snipe.png/revision/latest/scale-to-width-down/100?cb=20191011023719",
+        "Calibrate":"https://static.wikia.nocookie.net/nova-drift/images/7/71/Calibrate.png/revision/latest/scale-to-width-down/100?cb=20191011021717",
+        "Incendiary Strike":"https://static.wikia.nocookie.net/nova-drift/images/0/07/Incendiary_Strike.png/revision/latest/scale-to-width-down/100?cb=20191011022824"
+    },
+    "rate of fire":{
+        "Rapid Fire":"https://static.wikia.nocookie.net/nova-drift/images/4/4b/Rapid_Fire.png/revision/latest/scale-to-width-down/100?cb=20191011023405",
+        "Burst Fire":"https://static.wikia.nocookie.net/nova-drift/images/b/b0/Burst_Fire.png/revision/latest/scale-to-width-down/100?cb=20191011021713",
+        "Warpath":"https://static.wikia.nocookie.net/nova-drift/images/8/8c/Warpath.png/revision/latest/scale-to-width-down/100?cb=20191011023956",
+        "Siege Weaponry":"https://static.wikia.nocookie.net/nova-drift/images/c/ce/Siege_Weaponry.png/revision/latest/scale-to-width-down/100?cb=20191011023708"
+    },
+    "multiple projectiles":{
+        "Volley":"https://static.wikia.nocookie.net/nova-drift/images/5/5d/Volley.png/revision/latest/scale-to-width-down/100?cb=20191011023952",
+        "Focus Fire":"https://static.wikia.nocookie.net/nova-drift/images/0/0b/Focus_Fire.png/revision/latest/scale-to-width-down/100?cb=20191011022512",
+        "Firing Array":"https://static.wikia.nocookie.net/nova-drift/images/a/af/Firing_Array.png/revision/latest/scale-to-width-down/100?cb=20191011022454",
+        "Fusillade":"https://static.wikia.nocookie.net/nova-drift/images/3/39/Fusillade.png/revision/latest/scale-to-width-down/100?cb=20191011022747"
+    },
+    "damage":{
+        "Magnitude":"https://static.wikia.nocookie.net/nova-drift/images/5/5a/Magnitude.png/revision/latest/scale-to-width-down/100?cb=20191011023111",
+        "Payload":"https://static.wikia.nocookie.net/nova-drift/images/7/78/Payload.png/revision/latest/scale-to-width-down/100?cb=20191011023139",
+        "Splinter":"https://static.wikia.nocookie.net/nova-drift/images/7/70/Splinter.png/revision/latest/scale-to-width-down/100?cb=20191011023722",
+        "Charged Shot":"https://static.wikia.nocookie.net/nova-drift/images/7/7e/Charged_Shot.png/revision/latest/scale-to-width-down/100?cb=20191011022025"
+    },
+    "blast radius":{
+        "Blast Radius":"https://static.wikia.nocookie.net/nova-drift/images/4/4b/Blast_Radius.png/revision/latest/scale-to-width-down/100?cb=20191011021659",
+        "High Explosive":"https://static.wikia.nocookie.net/nova-drift/images/3/3e/High_Explosive.png/revision/latest/scale-to-width-down/100?cb=20191011022806",
+        "Concentrated Blast":"https://static.wikia.nocookie.net/nova-drift/images/5/5b/Concentrated_Blast.png/revision/latest/scale-to-width-down/100?cb=20191011022030",
+        "Rupture":"https://static.wikia.nocookie.net/nova-drift/images/3/38/Rupture.png/revision/latest/scale-to-width-down/100?cb=20191011023453"
+    },
+    "targeting":{
+        "Targeting":"https://static.wikia.nocookie.net/nova-drift/images/b/be/Targeting.png/revision/latest/scale-to-width-down/100?cb=20191011023748",
+        "Guidance":"https://static.wikia.nocookie.net/nova-drift/images/6/69/Guidance.png/revision/latest/scale-to-width-down/100?cb=20191011022802",
+        "Homing Strike":"https://static.wikia.nocookie.net/nova-drift/images/f/f9/Homing_Strike.png/revision/latest/scale-to-width-down/100?cb=20191011022811",
+        "Convergence":"https://static.wikia.nocookie.net/nova-drift/images/d/d0/Convergence.png/revision/latest/scale-to-width-down/100?cb=20191011022035"
+    },
+    "hull":{
+        "Hull Strength":"https://static.wikia.nocookie.net/nova-drift/images/4/40/Hull_Strength.png/revision/latest/scale-to-width-down/100?cb=20191011022815",
+        "Absorption":"https://static.wikia.nocookie.net/nova-drift/images/d/db/Absorption.png/revision/latest/scale-to-width-down/100?cb=20191011021253",
+        "Juggernaut":"https://static.wikia.nocookie.net/nova-drift/images/0/07/Juggernaut.png/revision/latest/scale-to-width-down/100?cb=20191011023057",
+        "Force Armor":"https://static.wikia.nocookie.net/nova-drift/images/d/de/Force_Armor.png/revision/latest/scale-to-width-down/100?cb=20191011022520"
+    },
+    "regen":{
+        "Regeneration":"https://static.wikia.nocookie.net/nova-drift/images/5/53/Regeneration.png/revision/latest/scale-to-width-down/100?cb=20191011023426",
+        "Regenerative Shields":"https://static.wikia.nocookie.net/nova-drift/images/b/bd/Regenerative_Shields.png/revision/latest/scale-to-width-down/100?cb=20191011023431",
+        "Rapid Reconstruction":"https://static.wikia.nocookie.net/nova-drift/images/f/f9/Rapid_Reconstruction.png/revision/latest/scale-to-width-down/100?cb=20191011023410",
+        "Adrenal Module":"https://static.wikia.nocookie.net/nova-drift/images/a/a1/Adrenal_Module.png/revision/latest/scale-to-width-down/100?cb=20191011021305"
+    },
+    "mobility 2":{
+        "Improved Thrusters":"https://static.wikia.nocookie.net/nova-drift/images/e/ec/Improved_Thrusters.png/revision/latest/scale-to-width-down/100?cb=20191011022819",
+        "Deadly Wake":"https://static.wikia.nocookie.net/nova-drift/images/8/83/Deadly_Wake.png/revision/latest/scale-to-width-down/100?cb=20191011022058",
+        "Streamline":"https://static.wikia.nocookie.net/nova-drift/images/6/64/Streamline.png/revision/latest/scale-to-width-down/100?cb=20191011023736",
+        "Blink":"https://static.wikia.nocookie.net/nova-drift/images/1/16/Blink.png/revision/latest/scale-to-width-down/100?cb=20191011021703"
+    },
+    "shield durability":{
+        "Shield Durability":"https://static.wikia.nocookie.net/nova-drift/images/9/99/Shield_Durability.png/revision/latest/scale-to-width-down/100?cb=20191011023653",
+        "Reflexive Shields":"https://static.wikia.nocookie.net/nova-drift/images/b/bf/Reflexive_Shields.png/revision/latest/scale-to-width-down/100?cb=20191011023420",
+        "Barrier":"https://static.wikia.nocookie.net/nova-drift/images/c/cc/Barrier.png/revision/latest/scale-to-width-down/100?cb=20191011021654",
+        "Omni Shield":"https://static.wikia.nocookie.net/nova-drift/images/4/40/Omni_Shield.png/revision/latest/scale-to-width-down/100?cb=20191011023125"
+    },
+    "shield 2":{
+        "Shield Cooldown":"https://static.wikia.nocookie.net/nova-drift/images/e/ed/Shield_Cooldown.png/revision/latest/scale-to-width-down/100?cb=20191011023649",
+        "Volatile Shields":"https://static.wikia.nocookie.net/nova-drift/images/5/50/Volatile_Shields.png/revision/latest/scale-to-width-down/100?cb=20191011023947",
+        "Flash Shielding":"https://static.wikia.nocookie.net/nova-drift/images/5/5c/Flash_Shielding.png/revision/latest/scale-to-width-down/100?cb=20191011022459",
+        "Discharge":"https://static.wikia.nocookie.net/nova-drift/images/c/c8/Discharge.png/revision/latest/scale-to-width-down/100?cb=20191011022112"
+    },
+    "shield effect":{
+        "Shield Effect Radius":"https://static.wikia.nocookie.net/nova-drift/images/8/83/Shield_Effect_Radius.png/revision/latest/scale-to-width-down/100?cb=20191011023659",
+        "Radiant Shields":"https://static.wikia.nocookie.net/nova-drift/images/c/c8/Radiant_Shields.png/revision/latest/scale-to-width-down/100?cb=20191011023158",
+        "Focused Shields":"https://static.wikia.nocookie.net/nova-drift/images/e/e5/Focused_Shields.png/revision/latest/scale-to-width-down/100?cb=20191011022516",
+        "Weaponized Shields":"https://static.wikia.nocookie.net/nova-drift/images/9/95/Weaponized_Shields.png/revision/latest/scale-to-width-down/100?cb=20191011023959"
+    }
+}
+
+wild_mods={}
+super_mods={}
